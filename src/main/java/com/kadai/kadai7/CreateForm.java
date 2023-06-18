39d8a1ec524427769a3f812d396b50c218d5bf8c
hirakawa masaki <hirakawamasaki@hirakawanoiMac.local>
2023-06-18 22:31:11 +0900

    @PostMappingと@GetMappingを実装しました。

---
+++ src/main/java/com/kadai/kadai7/CreateForm.java
@@ -0,0 +1,24 @@
+package com.kadai.kadai7;
+
+public class CreateForm {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
